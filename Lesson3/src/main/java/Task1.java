130575f54af567422ec2973d68a69fd57f516883
Aloshya <alexis.belyakoff@yandex.ru>
2023-03-09 20:57:14 +0300

    Done Task1 Lesson 3

---
+++ Lesson3/src/main/java/Task1.java
@@ -0,0 +1,26 @@
+/**
+   1) Используя тип данных char и операцию инкремента вывести на консоль все буквы английского алфавита
+ */
+
+
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        char firstSymbol = 'a';
+        char lastSymbol = 'z';
+
+        for (int i = (int) firstSymbol; i <= (int) lastSymbol; i++) {
+            System.out.print((char)i + " ");
+
+        }
+
+        System.out.println();
+
+        for (char i = 'a'; i <= 'z'; i++) {
+
+            System.out.print(i + " ");
+        }
+    }
+}
