d15c9696bb12d0d37f5d1e82385dec8fb47115fb
Aloshya <alexis.belyakoff@yandex.ru>
2023-03-22 21:51:02 +0300

    Wrote Tests

---
+++ Lesson1/src/main/java/Task3.java
@@ -0,0 +1,9 @@
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int Sum(int a, int b) {
+        return a + b;
+    }
+}
---
+++ Lesson1/src/test/java/Task3Test.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Task3Test {
+
+    @org.junit.jupiter.api.Test
+    void sum() {
+        assertEquals(6, Task3.Sum(3, 3));
+    }
+}
