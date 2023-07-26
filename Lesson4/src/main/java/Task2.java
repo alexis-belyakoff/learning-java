a33ec16a2080bfc1cc0f68353ed8272fa3b7d97d
Aloshya <alexis.belyakoff@yandex.ru>
2023-07-26 19:52:20 +0300

    Done Task 1 Lesson 4

--- Lesson4/src/main/java/Task2.java
+++ Lesson4/src/main/java/Task2.java
@@ -1,35 +1,69 @@
 /**
- * Дан двухмерный массив размерностью 4 на 4, необходимо нарисовать четыре треугольника вида
- * <p>
- * a)                  b)
- * *        *
- * * *        * *
- * * * *        * * *
- * * * * *        * * * *
- * <p>
- * c)                  d)
- * * * * *        * * * *
- * * * *        * * *
- * * *        * *
- * *        *
+  Дан двухмерный массив размерностью 4 на 4, необходимо нарисовать четыре треугольника вида
+
+         a)                  b)
+               *        *
+             * *        * *
+           * * *        * * *
+         * * * *        * * * *
+
+         c)                  d)
+         * * * *        * * * *
+           * * *        * * *
+             * *        * *
+               *        *
  */
 
 public class Task2 {
 
     public static void main(String[] args) {
-        char[][] print = new char[4][4];
+        char[][] printC = new char[4][4];
+        char[][] printB = new char[4][4];
+        char[][] printA = new char[4][4];
+        char[][] printD = new char[4][4];
 
-        for (int i = 0; i < print.length; i++) {
-            for (int j = i; j < print.length; j++) {
-                print[j][i] += '*';
+        for (int i = 0; i < printA.length; i++) {
+            for (int j = printA.length - 1; j >= 0; j--) {
+                printA[i][j] += '*';
 
             }
 
         }
 
-        for (char[] arr : print) {
+        for (char[] arr : printA) {
             System.out.println(arr);
 
         }
+
+        System.out.println();
+
+        for (int i = printC.length; i >= 0; i--) {
+            for (int j = i; j < printC.length; j++) {
+                printC[i][j] += '*';
+
+            }
+
+        }
+
+        for (char[] arr : printC) {
+            System.out.println(arr);
+
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < printB.length; i++) {
+            for (int j = i; j >= 0; j--) {
+                printB[i][j] += '*';
+
+            }
+
+        }
+
+        for (char[] chars : printB) {
+                    System.out.println(chars);
+
+        }
+
     }
 }
