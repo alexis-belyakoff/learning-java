813c1da67c604d3159b6b5718cd70e1b79684916
Aloshya <alexis.belyakoff@yandex.ru>
2023-09-08 22:35:52 +0300

    Done Task 1, 2, 3, 4, 5, 6 Lesson 4

--- Lesson4/src/main/java/Task6.java
+++ Lesson4/src/main/java/Task6.java
@@ -1,13 +1,19 @@
+import java.util.Arrays;
+
 public class Task6 {
     public static void main(String[] args) {
+
+
         int[] nums = new int[100];
+
+
         for (int i = 0; i <= nums.length; i++) {
             if (i % 2 != 0) {
                 nums[i] += i;
             }
 
         }
-        for (int i = 0; i < nums.length; i++) {
+        /*for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0 && i != 99) {
                 System.out.print(nums[i] + ", ");
             } else if (nums[i] != 0 && i == 99) {
@@ -22,6 +28,24 @@ public class Task6 {
             } else if (nums[i] != 0 && i == 1) {
                 System.out.print(nums[i] + ".");
             }
+        }*/
+        //sort(nums);
+        Arrays.sort(nums);
+        int[] arr = Arrays.copyOfRange(nums, findPos(nums), nums.length);
+        System.out.println(Arrays.toString(arr));
+
+
+
+    }
+
+    public static int findPos(int[] nums) {
+        int pos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 1) {
+                pos++;
+            }
+
         }
+        return pos;
     }
 }
